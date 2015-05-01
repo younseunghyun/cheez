421c864a9a9f6a790059a18b5467b31dad4cbaa4
jiho <polaris341@gmail.com>
2015-05-01 13:30:32 +0900

    슬랙 깃헙 연동 테스트

--- cheez/cheez/tests.py
+++ cheez/cheez/tests.py
@@ -1,8 +1,34 @@
 from rest_framework.test import APILiveServerTestCase
 from users.user import (
+    Device,
+    SNSAccount,
     User,
-
 )
 
-class CreateModelsTestCase(APILiveServerTestCase):
+class BaseTestCase(APILiveServerTestCase):
+    def setUp(self):
+        self.create_users()
+        self.create_posts()
+
+
+
+
+    def create_users(self):
+        prev_user_count = User.objects.count()
+
+        # create user with email password
+        user = User(
+            email='s_polaris@naver.com',
+        )
+        user.set_password('some_password')
+        user.save()
+
+        self.assertEqual(prev_user_count+1, User.objects.count())
+
+
+
+
+    def create_posts(self):
+        pass
+
 
